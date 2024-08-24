156b413f16ac7dfda7b48e3f7136c7a50dc7a5a6
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-24 03:08:31 -0400

    Implement mobile navbar

--- components/MainPageComponents/Navbar.js
+++ components/MainPageComponents/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {
     Navbar,
-    NavbarBrand, 
+    NavbarBrand,
     NavbarContent,
     NavbarItem,
     Button,
@@ -34,6 +34,7 @@ export default function NavBar() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [profilePicture, setProfilePicture] = useState([]);
+    const [displaySearchBar, setDisplaySearchBar] = useState(false);
 
     const router = useRouter();
     let { uid } = router.query; // query against url for uid 
@@ -93,11 +94,16 @@ export default function NavBar() {
 
     return (
         <Navbar isBordered>
-            <NavbarBrand>
+            <NavbarBrand className="visible max-sm:hidden">
                 <Button variant="light" className="text-blue-800 text-xl font-bold">
                     Socialkick
                 </Button>
             </NavbarBrand>
+            <NavbarContent>
+                <Button variant="light" className="bg-blue-100" text-blue-800 onClick={() => setDisplaySearchBar(!displaySearchBar)}>
+                    <SearchIcon />
+                </Button>
+            </NavbarContent>
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
                     <Input
@@ -123,68 +129,90 @@ export default function NavBar() {
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem>
-                    <Button variant="light" className="bg-blue-100 rounded-5">
-                        <UploadIcon className="w-[1.7em] " />
+                    <Button variant="light" className={`bg-blue-100 rounded-5 ${displaySearchBar ? "hidden" : "flex"}`}>
+                        <UploadIcon className="w-[1.2em] md:w-[1.7em] " />
                     </Button>
                 </NavbarItem>
                 <NavbarItem>
-                    <Dropdown showArrow className="border-2 border-default-200 drop-shadow-xl">
-                        <DropdownTrigger>
-                            <Avatar src={profilePicture} />
-                        </DropdownTrigger>
-                        <DropdownMenu aria-label="User menu">
-                            <DropdownSection title="Account" showDivider>
-                                <DropdownItem key="username" textValue={username}>
-                                    <User name={username} description={email} avatarProps={{ src: profilePicture }} />
-                                </DropdownItem>
-                            </DropdownSection>
-                            <DropdownSection title="Management" showDivider>
-                                <DropdownItem>
-                                    <Link href={`/${uid}/profile/Profile`}>
+                    {displaySearchBar ?
+                        <div className="flex mx-auto">
+                            <Input
+                                type="search"
+                                placeholder="Search"
+                                className="w-[150px] focus:text-default-500"
+                                color="primary"
+                                size="2xl"
+                                startContent={<SearchIcon />}
+                                classNames={{
+                                    input: [
+                                        "text-blue-800",
+                                        "placeholder:text-blue-800",
+                                        "focus:text-blue-800",
+                                    ],
+                                }}
+                            />
 
-                                        <p className="flex items-center gap-2">
-                                            <ProfileIcon />
-                                            Profile
-                                        </p>
-                                    </Link>
-                                </DropdownItem>
-                                <DropdownItem className="flex">
-                                    <Link href={`/${uid}/profile/Settings`}>
-                                        <p className="flex items-center gap-2">
-                                            <SettingsIcon />
-                                            Settings
-                                        </p>
-                                    </Link>
-                                </DropdownItem>
-                                <DropdownItem>
-                                    <Link href={`/${uid}/profile/Notifications`}>
-                                        <p className="flex items-center gap-2">
-                                            <NotificationsIcon />
-                                            Notifications
-                                        </p>
-                                    </Link>
-                                </DropdownItem>
-                                <DropdownItem>
-                                    <Link href="/HelpCenter">
-                                        <p className="flex items-center gap-2">
-                                            <HelpIcon />
-                                            Help Center
-                                        </p>
-                                    </Link>
-                                </DropdownItem>
-                            </DropdownSection>
-                            <DropdownSection>
-                                <DropdownItem onClick={doSignOut}>
-                                    <Link href="/">
-                                        <p className="text-red-600 flex items-center gap-2">
-                                            <SignOutIcon />
-                                            Sign Out
-                                        </p>
-                                    </Link>
-                                </DropdownItem>
-                            </DropdownSection>
-                        </DropdownMenu>
-                    </Dropdown>
+                        </div>
+                        :
+
+                        <Dropdown showArrow className={`border-2 border-default-200 drop-shadow-xl ${displaySearchBar ? "hidden" : "flex"}`}>
+                            <DropdownTrigger>
+                                <Avatar className={`${displaySearchBar ? "hidden" : "flex"}`} src={profilePicture} />
+                            </DropdownTrigger>
+                            <DropdownMenu aria-label="User menu">
+                                <DropdownSection title="Account" showDivider>
+                                    <DropdownItem key="username" textValue={username}>
+                                        <User name={username} description={email} avatarProps={{ src: profilePicture }} />
+                                    </DropdownItem>
+                                </DropdownSection>
+                                <DropdownSection title="Management" showDivider>
+                                    <DropdownItem>
+                                        <Link href={`/${uid}/profile/Profile`}>
+
+                                            <p className="flex items-center gap-2">
+                                                <ProfileIcon />
+                                                Profile
+                                            </p>
+                                        </Link>
+                                    </DropdownItem>
+                                    <DropdownItem className="flex">
+                                        <Link href={`/${uid}/profile/Settings`}>
+                                            <p className="flex items-center gap-2">
+                                                <SettingsIcon />
+                                                Settings
+                                            </p>
+                                        </Link>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <Link href={`/${uid}/profile/Notifications`}>
+                                            <p className="flex items-center gap-2">
+                                                <NotificationsIcon />
+                                                Notifications
+                                            </p>
+                                        </Link>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <Link href="/HelpCenter">
+                                            <p className="flex items-center gap-2">
+                                                <HelpIcon />
+                                                Help Center
+                                            </p>
+                                        </Link>
+                                    </DropdownItem>
+                                </DropdownSection>
+                                <DropdownSection>
+                                    <DropdownItem onClick={doSignOut}>
+                                        <Link href="/">
+                                            <p className="text-red-600 flex items-center gap-2">
+                                                <SignOutIcon />
+                                                Sign Out
+                                            </p>
+                                        </Link>
+                                    </DropdownItem>
+                                </DropdownSection>
+                            </DropdownMenu>
+                        </Dropdown>
+                    }
                 </NavbarItem>
             </NavbarContent>
         </Navbar>
