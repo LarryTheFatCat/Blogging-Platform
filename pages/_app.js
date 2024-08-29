72028ecb04161ae13fb9f3f052741ceb9d05c097
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-29 14:34:45 -0400

    Loading state

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import "@/styles/globals.css";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Loading from "./Loading";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
+  return loading ? <Loading /> : <Component {...pageProps} />;
 }
+
+export default MyApp;
