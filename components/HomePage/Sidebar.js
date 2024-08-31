6d54fbeb6197241c7df28476293f3e85cf91d0bd
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-31 19:50:14 -0400

    Mobile design

--- components/HomePage/Sidebar.js
+++ components/HomePage/Sidebar.js
@@ -1,8 +1,8 @@
 import { auth } from "@/utils/firebase";
-import { avatar, Button, Card, CardBody, CardHeader, Divider, User } from "@nextui-org/react";
+import { Avatar, Button, Card, CardBody, CardHeader, Divider, Popover, PopoverTrigger, PopoverContent, User } from "@nextui-org/react";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { FaUser, FaHome, FaCompass, FaList, FaBookmark, FaCog } from 'react-icons/fa';
+import { FaUser, FaHome, FaCompass, FaList, FaBookmark, FaCog, FaBars } from 'react-icons/fa';
 
 export default function Sidebar({ onItemSelect }) {
     const [avatarURL, setAvatarURL] = useState(null);
@@ -10,6 +10,7 @@ export default function Sidebar({ onItemSelect }) {
         username: "",
         email: "",
     });
+    const [isOpen, setIsOpen] = useState(false);
     const sideBarItems = [
         { name: "Home", icon: FaHome },
         { name: "Profile", icon: FaUser },
@@ -41,31 +42,73 @@ export default function Sidebar({ onItemSelect }) {
         return () => unsubscribe();
     }, []);
     return (
-        <div className="flex items-center h-screen">
-            <Card className="w-[500px] p-10 align-middle fixed">
-                <CardHeader className="grid grid-cols-1">
-                    <div >
-                        <h1 className="text-xl text-gray-400 font-thin">
-                            Welcome {userInfo.username}
-                        </h1>
-                    </div>
-                    <User className="justify-start pt-5 pl-3 pb-3" avatarProps={{ src: avatarURL }} name={userInfo.username} description={userInfo.email} />
-                </CardHeader>
-                <Divider />
-                <CardBody className="pt-3 gap-y-5">
-                    {sideBarItems.map((item) => (
-                        <Button
-                            key={item.name}
-                            variant="light"
-                            className="justify-start"
-                            onClick={() => onItemSelect(item.name)}
-                        >
-                            <item.icon className="mr-2" />
-                            {item.name}
+        <>
+            {/* Desktop Sidebar */}
+            <div className="hidden lg:flex items-center h-screen">
+                <Card className="w-[500px] p-10 align-middle fixed">
+                    <CardHeader className="grid grid-cols-1">
+                        <div>
+                            <h1 className="text-xl text-gray-400 font-thin">
+                                Welcome {userInfo.username}
+                            </h1>
+                        </div>
+                        <User className="justify-start pt-5 pl-3 pb-3" avatarProps={{ src: avatarURL }} name={userInfo.username} description={userInfo.email} />
+                    </CardHeader>
+                    <Divider />
+                    <CardBody className="pt-3 gap-y-5">
+                        {sideBarItems.map((item) => (
+                            <Button
+                                key={item.name}
+                                variant="light"
+                                className="justify-start"
+                                onClick={() => onItemSelect(item.name)}
+                            >
+                                <item.icon className="mr-2" />
+                                {item.name}
+                            </Button>
+                        ))}
+                    </CardBody>
+                </Card>
+            </div>
+
+            <div className="lg:hidden">
+                <Popover isOpen={isOpen} onOpenChange={setIsOpen} placement="left">
+                    <PopoverTrigger>
+                        <Button isIconOnly aria-label="Open Sidebar" className="fixed top-4 left-4">
+                            <FaBars />
                         </Button>
-                    ))}
-                </CardBody>
-            </Card>
-        </div>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <Card className="w-[300px] p-4">
+                            <CardHeader className="grid grid-cols-1">
+                                <div>
+                                    <h1 className="text-xl text-gray-400 font-thin">
+                                        Welcome {userInfo.username}
+                                    </h1>
+                                </div>
+                                <User className="justify-start pt-5 pl-3 pb-3" avatarProps={{ src: avatarURL }} name={userInfo.username} description={userInfo.email} />
+                            </CardHeader>
+                            <Divider />
+                            <CardBody className="pt-3 gap-y-5">
+                                {sideBarItems.map((item) => (
+                                    <Button
+                                        key={item.name}
+                                        variant="light"
+                                        className="justify-start"
+                                        onClick={() => {
+                                            onItemSelect(item.name);
+                                            setIsOpen(false);
+                                        }}
+                                    >
+                                        <item.icon className="mr-2" />
+                                        {item.name}
+                                    </Button>
+                                ))}
+                            </CardBody>
+                        </Card>
+                    </PopoverContent>
+                </Popover>
+            </div>
+        </>
     )
 }
